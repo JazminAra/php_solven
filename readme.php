9d0a2564cc2ecaa91374f4740678d1559b815aa1
JazminAra <134907908+JazminAra@users.noreply.github.com>
2024-11-24 22:29:26 -0500

    Update readme.php

--- readme.php
+++ readme.php
@@ -1,3 +1,7 @@
+# SYSTEM IN PHP
+
+Web system for loaning materials in a university in PHP with CRUD modules for students, users, administrators, articles, materials, locations, faculties, schools, offices and office types.
+
 # SISTEMA EN PHP
 
 Sistema web de solvencia para prestamos de materiales en una universidad en php con modulos de CRUD de alumnos, usuarios, administrativos, arituclo, materiales, sedes, facultades, escuelas, oficina y tipo oficina. 
